5da33c071c8d7f52c15ccb10f63cc282ad8e3322
Cd246810 <christian246810@gmail.com>
2017-11-04 16:04:06 -0600

    s

--- index.php
+++ index.php
@@ -98,7 +98,7 @@ function Hijos($arregloFichas,$turno){
 				if($fila<6 && $columna>1){
 					$nuevaFila=diagonalAbajoIzquierda($arregloFichas,$turno,$fila+1,$columna-1);
 					if($nuevaFila!=-1){
-						echo ('<br>Posicion: '.$columna.' '.$fila.' Se puede a mover en diagonal abajo izquierda a la posicion: '.$nuevaFila);
+						echo ('<br>Posicion: '.$columna.' '.$fila.' Se puede a mover en diagonal abajo izquierda a la posicion: '.($columna-$nuevaFila+$fila).$nuevaFila);
 					}
 				}
 				if($fila<6 && $columna<6){
