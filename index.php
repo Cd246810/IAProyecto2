50887964e8208f4d06b7b7b842c403a98395a07b
Cd246810 <christian246810@gmail.com>
2017-11-05 10:40:52 -0600

    Con2

--- index.php
+++ index.php
@@ -422,7 +422,7 @@ function backtracking($raiz,$turno){
 	while($lista){
 		$nodoActual=array_shift($lista);
 		$visitado[]=$nodoActual;
-		if($nodoActual[4]<1){
+		if($nodoActual[4]<2){
 			$hijos=array();
 			$hijos=Hijos($nodoActual[0],$turno,$nodoActual[4]);
 			foreach ($hijos as $hijo) {
